d0413d72ab3d9aeb6ddeda54d18b7a47768b254c
Mimish Vader13 <garmart13>
2018-09-19 20:56:57 -0500

    se realizaron funciones en botones

--- app/src/main/java/unam/fca/dmoviles/MainActivity.java
+++ app/src/main/java/unam/fca/dmoviles/MainActivity.java
@@ -2,40 +2,29 @@ package unam.fca.dmoviles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.LocalDateTime;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView texto;
+    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Actividad 2 Introduccion a eventos con Button
-        //https://developer.android.com/reference/android/widget/Button
-        //Button es un componente de Android que realizar acciones al hacer click en el
-
-        // El archivo del layout de la actividad se encuentra en la carpeta
-        // res/layout/activity_main.xml
         setContentView(R.layout.activity_main);
 
+        texto = (TextView) findViewById(R.id.texto);
+        btn = (Button) findViewById(R.id.btn);
 
-        //Para hacer referencia a un componente del layout, utilizamos el metodo findViewById
-        //y le pasamos como parametro el id correspondiente.
+    }
 
-        final TextView tv = findViewById(R.id.btn);
-
-        final Button btn = findViewById(R.id.btn);
-
-
-        //Para cambiar el texto de tv
-        tv.setText("Hola mundo");
-
-        //Actividades
-        //a) Agrega un evento  setOnClickListener al boton como viene en la referencia
-        //b) El el metodo onClick, modifica el texto de tv para que muestre la fecha actual con LocalDateTime.now().ToString()
-        //c) Sube tu codigo al repositorio.
-        //d) Sube un documento en word a la plataforma Moodle con las capturas de pantalla de tu actividad. Incluye la liga a tu repositorio
-
+    @Override
+    public void onClick(View view) {
+        btn.setText(LocalDateTime.now().toString());
     }
 }
